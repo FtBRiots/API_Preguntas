0acd3beb88fc30dd502761f47ea75227612f7fbe
FtBRiots <dany94188secundario@gmail.com>
2022-10-21 16:38:39 -0500

    Sabe commit

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
-    host: 'dpg-cd9ge9aen0hldb1s20f0-a',
-    user: 'root',
-    password: 'rxVjI1oQ9CYMAkGRKJj3x4fQvDZAy88G',
+    host: 'us-east.connect.psdb.cloud',
+    user: 'wt12fl1zrpyrxtn7f0nr',
+    password: 'pscale_pw_TJU5bGv8KSzqaCY3ON5Hnl2QJZWWFDd6ZqMlvuwHBfT',
     database: 'quizpaa'
 });
 
