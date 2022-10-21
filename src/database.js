e44637c02a37e6f712f7b1b793883513ae3cc047
FtBRiots <dany94188secundario@gmail.com>
2022-10-21 16:02:07 -0500

    Second commit

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
+    host: 'dpg-cd9ge9aen0hldb1s20f0-a',
     user: 'root',
-    password: '',
+    password: 'rxVjI1oQ9CYMAkGRKJj3x4fQvDZAy88G',
     database: 'quizpaa'
 });
 
